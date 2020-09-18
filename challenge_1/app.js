82ea70ac0b25a032ba8c0e3d9cffbe3fbb25c2da
twzkraus <twzkraus@gmail.com>
2020-09-18 14:09:09 -0700

    basic gameplay implemented. X's and O's show up, and 'game over' is logged when game is over.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -2,15 +2,15 @@
 const players = ['X', 'O'];
 let currentPlayerIdx = 0;
 // board status
-const board = [
-  [[], [], []],
-  [[], [], []],
-  [[], [], []]
-];
+const board = ['', '', '', '', '', '', '', '', ''];
+const boardDivs = new Array(9);
 // game-over
 let gameOver = false;
 
 const boardElement = document.getElementById("board");
+for (let i = 0; i < 9; i++) {
+  boardDivs[i] = document.getElementById(`box${i}`);
+}
 
 // high level:
   // while !gameOver
@@ -24,21 +24,59 @@ HELPER FUNCTIONS
 // click handler
 boardElement.addEventListener('click', (event) => {
   // note: use "path" on event to figure out which element was clicked
-  let boxNum = event.target.className[event.target.className.length - 1];
-  console.log('box number', boxNum, 'was clicked!');
+  // debugger;
+  let boxNum = event.target.id[event.target.id.length - 1];
+  handleBoxClicked(boxNum);
 });
+
+// gameplay handler of box clicked
+handleBoxClicked = (boxNumber) => {
+  if (!board[boxNumber]) {
+    board[boxNumber] = players[currentPlayerIdx];
+    render();
+    checkIfGameOver();
+    currentPlayerIdx = (currentPlayerIdx + 1) % 2;
+  }
+  // possible improvement: add an else and message of 'please select a new box'
+}
 // render
-render = () => {};
+render = () => {
+  // loop over board array, make it match the html
+  board.forEach((boxVal, index) => {
+    if (boxVal) {
+      // render on the dom
+      boardDivs[index].innerHTML = boxVal;
+    }
+  });
+};
 // checkIfGameOver
-checkIfGameOver = () => {};
+checkIfGameOver = () => {
+  let idxsToCheck = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  idxsToCheck.forEach(combo => {
+    let i = combo[0];
+    let j = combo[1];
+    let k = combo[2];
+    if (board[i] && board[i] === board[j] && board[j] === board[k]) {
+      console.log('game over!');
+      gameOver = true;
+    }
+  });
+};
 
 /**************
 MAIN GAMEPLAY
 **************/
 
-while (!gameOver) {
-  render();
-  checkIfGameOver();
-  currentPlayerIdx = (currentPlayerIdx + 1) % 2;
-  gameOver = true;
-}
+
+
+
