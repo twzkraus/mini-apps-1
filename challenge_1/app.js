f1628753ba5ed91507e0fdb6c3f6b2bac1727e8e
twzkraus <twzkraus@gmail.com>
2020-09-18 12:31:42 -0700

    created grid. Added logic to identify which box was clicked.

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,44 @@
-console.log('in the app file');
+// current player
+const players = ['X', 'O'];
+let currentPlayerIdx = 0;
+// board status
+const board = [
+  [[], [], []],
+  [[], [], []],
+  [[], [], []]
+];
+// game-over
+let gameOver = false;
 
+const boardElement = document.getElementById("board");
+
+// high level:
+  // while !gameOver
+  // render board
+  // alternate turns for X and O
+    // click handlers need to be in place
+    // check whether gameOver
+/**************
+HELPER FUNCTIONS
+**************/
+// click handler
+boardElement.addEventListener('click', (event) => {
+  // note: use "path" on event to figure out which element was clicked
+  let boxNum = event.target.className[event.target.className.length - 1];
+  console.log('box number', boxNum, 'was clicked!');
+});
+// render
+render = () => {};
+// checkIfGameOver
+checkIfGameOver = () => {};
+
+/**************
+MAIN GAMEPLAY
+**************/
+
+while (!gameOver) {
+  render();
+  checkIfGameOver();
+  currentPlayerIdx = (currentPlayerIdx + 1) % 2;
+  gameOver = true;
+}
