b954e49f1d796b07baf82d9e16fedde311d2ee9e
twzkraus <twzkraus@gmail.com>
2020-09-19 16:12:27 -0700

    form re-provided to user after submitting JSON

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -2,8 +2,56 @@
   // h1
   // form
     // textarea
-    // button
+    // submitButton
   // display area?
+const sampleJSON = {
+  "firstName": "Joshie",
+  "lastName": "Wyattson",
+  "county": "San Mateo",
+  "city": "San Mateo",
+  "role": "Broker",
+  "sales": 1000000,
+  "children": [
+  {
+    "firstName": "Beth Jr.",
+    "lastName": "Johnson",
+    "county": "San Mateo",
+    "city": "Pacifica",
+    "role": "Manager",
+    "sales": 2900000,
+    "children": [
+      {
+        "firstName": "Smitty",
+        "lastName": "Won",
+        "county": "San Mateo",
+        "city": "Redwood City",
+        "role": "Sales Person",
+        "sales": 4800000,
+        "children": []
+      },
+      {
+        "firstName": "Allen",
+        "lastName": "Price",
+        "county": "San Mateo",
+        "city": "Burlingame",
+        "role": "Sales Person",
+        "sales": 2500000,
+        "children": []
+      }
+    ]
+  },
+  {
+    "firstName": "Beth",
+    "lastName": "Johnson",
+    "county": "San Francisco",
+    "city": "San Francisco",
+    "role": "Broker/Sales Person",
+    "sales": 7500000,
+    "children": []
+  }
+]
+};
+
 const body = document.body;
 const pageTitle = document.createElement('h1');
 pageTitle.innerHTML = 'JSON-CSV Converter';
@@ -23,20 +71,29 @@ form.setAttribute('action', '/add_data');
 
 let textInput = document.createElement('textarea');
 textInput.setAttribute('name', 'textfield');
-textInput.setAttribute('value', 'default text');
+// textInput.setAttribute('value', JSON.stringify(sampleJSON));
+textInput.innerHTML = JSON.stringify(sampleJSON);
 
-let button = document.createElement('input');
-button.setAttribute('type', 'submit');
-button.setAttribute('value', 'Submit');
+let submitButton = document.createElement('input');
+submitButton.setAttribute('type', 'submit');
+submitButton.setAttribute('value', 'Submit');
 
 form.appendChild(textInput);
-form.appendChild(button);
+form.appendChild(submitButton);
 
 body.appendChild(form);
 
 
 
-// set up click handlers and form submission
+// set up event listeners
 
+// submitButton.addEventListener('click', (event) => {
+//   // so user is not redirected
+//   event.preventDefault();
+// });
 
+// form.addEventListener('success', (err, data) => {
+//   console.log('inside success listener');
+//   displayBox.innerHTML = data;
+// });
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const PORT = 3000;
 const bodyParser = require('body-parser');
+const path = require('path');
+let dataServed;
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,21 +15,30 @@ app.listen(PORT, () => {
 
 app.post('/add_data', (req, res) => {
   console.log(`Request of type ${req.method} was received to url ${req.url}`);
-  res.send(parseJSONToCSV(JSON.parse(req.body.textfield)));
+  dataServed = parseJSONToCSV(JSON.parse(req.body.textfield));
+  // debugger;
+  res.format({
+    html: () => {
+      res.send(addCSVtoHTML(dataServed));
+    },
+  });
 });
 
-parseJSONToCSV = (obj) => {
+app.get('/add_data', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+})
+
+const parseJSONToCSV = (obj) => {
   let headersArr = [];
   for (let key in obj) {
     if (key !== 'children') {
       headersArr.push(key);
     }
   }
-  debugger;
   return headersArr.join(',') + '\n' + parseSingleRow(obj);
 };
 
-parseSingleRow = (obj) => {
+const parseSingleRow = (obj) => {
   let thisRowArr = [];
   let nextRow = '';
   for (let key in obj) {
@@ -41,4 +52,25 @@ parseSingleRow = (obj) => {
   });
 
   return thisRowArr.join(',') + nextRow;
-}
+};
+
+const addCSVtoHTML = (csvData) => {
+  return `<!DOCTYPE html>
+<html>
+  <head>
+
+  </head>
+
+  <body>
+    <h1>JSON-CSV Converter</h1>
+    <nav><a href="/">Home</a></nav>
+    <div>
+      ${csvData}
+    </div>
+    <form method="post" action="/add_data">
+      <textarea name="textfield"></textarea>
+      <input type="submit" value="Submit"></input>
+    </form>
+
+</html>`
+};
