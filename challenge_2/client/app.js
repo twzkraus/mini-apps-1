be0716daa90e835c997f419fa4ddec71c09b7eaa
twzkraus <twzkraus@gmail.com>
2020-09-19 10:17:19 -0700

    basic express server up and running

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,8 @@
+/*
+The server must flatten the JSON hierarchy, mapping each item/object in the JSON to a single line of CSV report (see included sample output), where the keys of the JSON objects will be the columns of the CSV report.
+You may assume the JSON data has a regular structure and hierarchy (see included sample file). In other words, all sibling records at a particular level of the hierarchy will have the same set of properties, but child objects might not contain the same properties. In all cases, every property you encounter must be present in the final CSV output.
+You may also assume that child records in the JSON will always be in a property called `children`.
+*/
+
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const PORT = 3000;
 
+const app = express();
 
+app.listen(PORT, () => {
+  console.log(`express is listening on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+  console.log(`serving request type ${req.method} on port ${PORT}`);
+});
