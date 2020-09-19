668680dab7a65b4455e017e301478a950bfcdfe3
twzkraus <twzkraus@gmail.com>
2020-09-19 10:43:05 -0700

    basic form functionality implemented on client

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,8 +1,27 @@
-/*
-The server must flatten the JSON hierarchy, mapping each item/object in the JSON to a single line of CSV report (see included sample output), where the keys of the JSON objects will be the columns of the CSV report.
-You may assume the JSON data has a regular structure and hierarchy (see included sample file). In other words, all sibling records at a particular level of the hierarchy will have the same set of properties, but child objects might not contain the same properties. In all cases, every property you encounter must be present in the final CSV output.
-You may also assume that child records in the JSON will always be in a property called `children`.
-*/
+// set up dom elements-
+  // h1
+  // form
+    // textarea
+    // button
+  // display area?
+const body = document.body;
+const pageTitle = document.createElement('h1');
+pageTitle.innerHTML = 'JSON-CSV Converter';
+body.appendChild(pageTitle);
+
+const form = document.createElement('form');
+let textinput = document.createElement('textarea');
+let button = document.createElement('input');
+button.setAttribute('type', 'submit');
+form.setAttribute('action', '/api/data');
+
+form.appendChild(textinput);
+form.appendChild(button);
+body.appendChild(form);
+
+
+
+// set up click handlers and form submission
 
 
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -3,10 +3,12 @@ const PORT = 3000;
 
 const app = express();
 
+app.use(express.static('./client'));
+
 app.listen(PORT, () => {
   console.log(`express is listening on port ${PORT}`);
 });
 
-app.get('/', (req, res) => {
-  console.log(`serving request type ${req.method} on port ${PORT}`);
+app.get('/api/data', (req, res) => {
+  console.log(`Request of type ${req.method} was received!`)
 });
