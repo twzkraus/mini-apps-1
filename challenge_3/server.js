898bc50ad113d309521e3a58942a37fd6e8f4adb
twzkraus <twzkraus@gmail.com>
2020-09-20 15:07:07 -0700

    initialized node_modules and implemented basic html serving

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,10 @@
+const express = require('express');
 
+const PORT = 3000;
+const app = express();
 
+app.use(express.static('./public'));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at port ${PORT}`);
+});
